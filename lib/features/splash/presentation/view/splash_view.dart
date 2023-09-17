b3fd70f7dbcb569a7b1c81f8f8437a3c77e6436d
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-17 17:57:32 +0300

    create splash view

--- lib/features/splash/presentation/view/splash_view.dart
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_triad/core/resources/manager_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_triad/core/resources/manager_fonts.dart';
+import 'package:flutter_triad/core/resources/manager_strings.dart';
 import '../../../../core/resources/manager_assets.dart';
 import '../../../../core/resources/manager_sizes_util.dart';
 import '../../../../core/widgets/scaffold_with_background.dart';
@@ -10,15 +12,41 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return scaffoldWithImageBackground(
-      child: Center(
-        child: SvgPicture.asset(
-          ManagerAssets.logo,
-          colorFilter: const ColorFilter.mode(
-            ManagerColors.background,
-            BlendMode.darken,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Center(
+        child: SizedBox(
+          height: ManagerHeight.h80,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                ManagerAssets.qLogo,
+                fit: BoxFit.contain,
+
+                ),
+              SizedBox(width: ManagerWidth.w4),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SvgPicture.asset(
+                    ManagerAssets.uickMartLogo,
+                    fit: BoxFit.contain,
+                    width: ManagerWidth.w180,
+                  ),
+                  SizedBox(height: ManagerHeight.h2),
+                  Text(
+                    ManagerStrings.logoText,
+                    style: TextStyle(
+                      color: ManagerColors.primaryColor_,
+                      fontSize: ManagerFontSize.s13,
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-          width: ManagerWidth.w180,
         ),
       ),
     );
