c966ac1d9dbb6d77270fa77708ade2070833f440
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-17 23:37:20 +0300

    add arrow_forward_android icon

--- lib/core/resources/manager_icons.dart
+++ lib/core/resources/manager_icons.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class ManagerIcons {
   static IconData arrow_back_ios = Icons.arrow_back_ios;
   static IconData arrow_back_android = Icons.arrow_back;
+  static IconData arrow_forward_android = Icons.arrow_forward;
   static IconData visibility_off = Icons.visibility_off;
   static IconData visibility = Icons.visibility;
 }
