3fb2cdac15638a5d5abdb116d4b8e594b3c11fb8
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-18 18:45:24 +0300

    add icons

--- lib/core/resources/manager_icons.dart
+++ lib/core/resources/manager_icons.dart
@@ -5,6 +5,6 @@ class ManagerIcons {
   static IconData arrow_back_ios = Icons.arrow_back_ios;
   static IconData arrow_back_android = Icons.arrow_back;
   static IconData arrow_forward_android = Icons.arrow_forward;
-  static IconData visibility_off = Icons.visibility_off;
-  static IconData visibility = Icons.visibility;
+  static IconData visibility_off = Icons.visibility_off_outlined;
+  static IconData visibility = Icons.visibility_outlined;
 }
