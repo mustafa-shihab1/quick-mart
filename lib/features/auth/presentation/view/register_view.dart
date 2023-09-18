af0199df483a2ee6fe58df6260a4fa8d186e7873
mstfa_201 <mostafaa.sh2001@gmail.com>
2023-09-18 18:46:38 +0300

    create register view

--- lib/features/auth/presentation/view/register_view.dart
+++ lib/features/auth/presentation/view/register_view.dart
@@ -1,20 +1,19 @@
-import 'package:flutter_triad/config/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_triad/core/resources/manager_assets.dart';
 import 'package:flutter_triad/core/resources/manager_colors.dart';
 import 'package:flutter_triad/core/resources/manager_fonts.dart';
 import 'package:flutter_triad/core/resources/manager_sizes_util.dart';
 import 'package:flutter_triad/core/resources/manager_strings.dart';
 import 'package:flutter_triad/core/resources/manager_styles.dart';
 import 'package:flutter_triad/core/validator/validator.dart';
-import 'package:flutter_triad/core/widgets/checkbox_widget.dart';
 import 'package:flutter_triad/core/widgets/main_button.dart';
 import 'package:flutter_triad/core/widgets/scaffold_with_background.dart';
-import 'package:flutter_triad/core/widgets/text_field.dart';
 import 'package:flutter_triad/features/auth/presentation/controller/register_controller.dart';
+import 'package:flutter_triad/features/auth/presentation/widget/auth_text_field.dart';
 import 'package:flutter_triad/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../core/resources/manager_icons.dart';
-import '../widget/footer_auth.dart';
 import '../widget/top_part_auth.dart';
 
 class RegisterView extends StatelessWidget {
@@ -25,178 +24,218 @@ class RegisterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return scaffoldWithImageBackground(
-      child: Column(
-        children: [
-          partTopAuth(),
-          Expanded(
-            flex: Constants.authSecondPartFlex,
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: ManagerWidth.w20,
-                vertical: ManagerHeight.h4,
-              ),
-              decoration: BoxDecoration(
-                color: ManagerColors.backgroundForm,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(
-                    ManagerRadius.r40,
-                  ),
-                ),
-              ),
-              child: GetBuilder<RegisterController>(
-                builder: (controller) {
-                  return SingleChildScrollView(
-                    child: Form(
-                      key: controller.formKey,
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: ManagerHeight.h20,
-                          ),
-                          Text(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            partTopAuth(),
+            GetBuilder<RegisterController>(
+              builder: (controller) {
+                return SingleChildScrollView(
+                  child: Form(
+                    key: controller.formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: ManagerHeight.h24,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
                             ManagerStrings.signUp,
-                            style: getMediumTextStyle(
-                              fontSize: ManagerFontSize.s26,
-                              color: ManagerColors.black,
+                            style: getBoldTextStyle(
+                              fontSize: ManagerFontSize.s24,
+                              color: ManagerColors.primaryColor_,
+
                             ),
                           ),
-                          SizedBox(
-                            height: ManagerHeight.h16,
-                          ),
-                          textField(
-                            hintText: ManagerStrings.fullName,
-                            controller: controller.fullName,
-                            textInputType: TextInputType.name,
-                            obSecure: false,
-                            validator: (value) => _validator.validateFullName(
-                              value,
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h4,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                                ManagerStrings.alreadyHaveAccount,
+                                style: getRegularTextStyle(
+                                  fontSize: ManagerFontSize.s14,
+                                  color: ManagerColors.subTitleColor,
+                                ),
                             ),
-                          ),
-                          SizedBox(
-                            height: ManagerHeight.h16,
-                          ),
-                          textField(
-                            hintText: ManagerStrings.email,
-                            controller: controller.email,
-                            obSecure: false,
-                            textInputType: TextInputType.emailAddress,
-                            validator: (value) => _validator.validateEmail(
-                              value,
-                            ),
-                          ),
-                          SizedBox(
-                            height: ManagerHeight.h16,
-                          ),
-                          textField(
-                            hintText: ManagerStrings.phoneNum,
-                            controller: controller.phoneNumber,
-                            obSecure: false,
-                            textInputType: TextInputType.number,
-                            validator: (value) =>
-                                _validator.validatePhoneNumber(
-                              value,
-                            ),
-                          ),
-                          SizedBox(
-                            height: ManagerHeight.h16,
-                          ),
-                          textField(
-                            hintText: ManagerStrings.password,
-                            controller: controller.password,
-                            obSecure: controller.isObscurePassword,
-                            suffixIcon: IconButton(
-                              icon: controller.isObscurePassword
-                                  ? Icon(
-                                      ManagerIcons.visibility_off,
-                                color: ManagerColors.greyLight,
-                                    )
-                                  : Icon(
-                                      ManagerIcons.visibility,
-                                color: ManagerColors.primaryColor,
-                                    ),
-                              onPressed: () =>
-                                  controller.onChangeObscurePassword(),
-                            ),
-                            textInputType: TextInputType.text,
-                            validator: (value) => _validator.validatePassword(
-                              value,
-                            ),
-                          ),
-                          SizedBox(
-                            height: ManagerHeight.h16,
-                          ),
-                          textField(
-                            hintText: ManagerStrings.confirmPass,
-                            obSecure: controller.isObscureConfirmPassword,
-                            suffixIcon: IconButton(
-                              icon: controller.isObscureConfirmPassword
-                                  ? Icon(
-                                ManagerIcons.visibility_off,
-                                color: ManagerColors.greyLight,
-                              )
-                                  : Icon(
-                                ManagerIcons.visibility,
-                                color: ManagerColors.primaryColor,
-                              ),
-                              onPressed: () =>
-                                  controller.onChangeObscureConfirmPassword(),
-                            ),
-                            textInputType: TextInputType.text,
-                            controller: controller.confirmPassword,
-                            validator: (value) => _validator.confirmPassword(
-                              controller.password.text,
-                              value,
-                            ),
-                          ),
-                          SizedBox(
-                            height: ManagerHeight.h16,
-                          ),
-                          Container(
-                            width: double.infinity,
-                            child: checkBoxWidget(
-                              checkBoxName: ManagerStrings.checkBoxName,
-                              status: controller.check,
-                              onChanged: (status) {
-                                controller.onChange(
-                                  status!,
-                                );
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            height: ManagerHeight.h20,
-                          ),
-                          SizedBox(
-                            width: double.infinity,
-                            height: ManagerHeight.h48,
-                            child: mainButton(
+                            TextButton(
                               onPressed: () {
-                                controller.performRegister(context);
+                                Get.offNamed(Routes.login);
                               },
-                              buttonName: ManagerStrings.signUp,
+                              child: Text(
+                                ManagerStrings.login,
+                                style: getRegularTextStyle(
+                                  fontSize: ManagerFontSize.s14,
+                                  color: ManagerColors.secondaryColor_,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h30,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              ManagerStrings.fullName,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.primaryColor_,
+                              ),
+                            ),
+                            Text(
+                              ManagerStrings.requiredStar,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.requiredStarColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h8,
+                        ),
+                        AuthTextField(
+                          hintText: ManagerStrings.fullName,
+                          obscureText: false,
+                          validator: (value) => _validator.validateFullName(
+                            value!,
+                          ),
+                          controller: controller.fullName,
+                          textInputType: TextInputType.name,
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h16,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              ManagerStrings.email,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.primaryColor_,
+                              ),
+                            ),
+                            Text(
+                              ManagerStrings.requiredStar,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.requiredStarColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h8,
+                        ),
+                        AuthTextField(
+                          hintText: ManagerStrings.email,
+                          obscureText: false,
+                          validator: (value) => _validator.validateEmail(
+                            value!,
+                          ),
+                          controller: controller.email,
+                          textInputType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h16,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              ManagerStrings.password,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.primaryColor_,
+                              ),
+                            ),
+                            Text(
+                              ManagerStrings.requiredStar,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.requiredStarColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h8,
+                        ),
+                        AuthTextField(
+                          hintText: ManagerStrings.enterYourPassword,
+                          obscureText: false,
+                          validator: (value) => _validator.validatePassword(
+                            value!,
+                          ),
+                          controller: controller.password,
+                          textInputType: TextInputType.text,
+                          suffixIcon: IconButton(
+                            icon: controller.isObscurePassword
+                                ? Icon(
+                              ManagerIcons.visibility_off,
+                              color: ManagerColors.greyLight,
+                            )
+                                : Icon(
+                              ManagerIcons.visibility,
+                              color: ManagerColors.primaryColor,
+                            ),
+                            onPressed: () =>
+                                controller.onChangeObscurePassword(),
+                          ),
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h24,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: ManagerHeight.h60,
+                          child: mainButton(
+                            onPressed: () {
+                              controller.performRegister(context);
+                            },
+                            child: Text(
+                              ManagerStrings.createAccount,
+                              style: getRegularTextStyle(
+                                fontSize: ManagerFontSize.s14,
+                                color: ManagerColors.white,
+                              ),
                             ),
                           ),
-                          SizedBox(
-                            height: ManagerHeight.h20,
+                        ),
+                        SizedBox(
+                          height: ManagerHeight.h30,
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                ManagerStrings.signupWithGoogle,
+                                style: getRegularTextStyle(
+                                  fontSize: ManagerFontSize.s14,
+                                  color: ManagerColors.primaryColor_,
+                                ),
+                              ),
+                              SizedBox(
+                                width: ManagerWidth.w4,
+                              ),
+                              SvgPicture.asset(ManagerAssets.google)
+                            ],
                           ),
-                          FooterAuth(
-                            text: ManagerStrings.footerSignUp,
-                            buttonName: ManagerStrings.login,
-                            onPressed: () {
-                              Get.offAllNamed(
-                                Routes.login,
-                              );
-                            },
-                          ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       isRegisterView: true,
     );
